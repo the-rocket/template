f3b1e1b3f2c3cf0e180c75d4fe6d62027cf9c643
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2017-04-09 00:35:37 -0500

    js precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-#Rails.application.config.assets.precompile += %w( modernizr-2.6.2-respond-1.1.0.min.js  query.min.js )
+Rails.application.config.assets.precompile += ['*.js', '*.css']
 #config.assets.compile = true
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
