b060a9e81b30c2f0ee967fc9793bc5c53c7e2f22
Daniyar Kayirbolatov <daniyarkayirbolatov@gmail.com>
2017-04-10 22:04:26 -0500

    js folder link fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,7 +2,9 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += ['*.js', '*.css']
+Rails.application.config.assets.precompile += %w(jquery.min.js jquery.plugin.min.js bootstrap.min.js 
+jquery.flexslider-min.js smooth-scroll.min.js skrollr.min.js spectragram.min.js scrollReveal.min.js isotope.min.js linkedinFetcher_v10_min.js  lightbox.min.js
+jquery.countdown.min.js scripts.js )
 #config.assets.compile = true
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
